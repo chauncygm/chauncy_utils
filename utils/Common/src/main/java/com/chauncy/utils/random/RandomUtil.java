4ba5bc67f78d4ecb5c4a2a269654ad9f7dc84219
gsj <704311761@qq.com>
2025-03-10 20:39:11 +0800

    随机工具类

--- utils/Common/src/main/java/com/chauncy/utils/random/RandomUtil.java
+++ utils/Common/src/main/java/com/chauncy/utils/random/RandomUtil.java
@@ -1,34 +1,36 @@
 package com.chauncy.utils.random;
 
-import org.apache.commons.lang3.exception.UncheckedException;
-
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 
 /**
  * 随机工具类
  *
  * @author chauncy
  */
+@ThreadSafe
 public class RandomUtil {
 
-    /** ThreadLocalRandom实例 */
-    public static RandomUtil INSECURE = new RandomUtil(ThreadLocalRandom::current);
+    public static final ThreadLocal<Random> LOCAL_RANDOM = ThreadLocal.withInitial(Random::new);
 
     private static final ThreadLocal<SecureRandom> SECURE_RANDOM_STRONG = ThreadLocal.withInitial(() -> {
         try {
             return SecureRandom.getInstanceStrong();
         } catch (final NoSuchAlgorithmException e) {
-            throw new UncheckedException(e);
+            throw new RuntimeException(e);
         }
     });
-    private final static Supplier<Random> SECURE_RANDOM_SUPPLIER = SECURE_RANDOM_STRONG::get;
+
+    /** Random实例 */
+    public static RandomUtil INSECURE = new RandomUtil(LOCAL_RANDOM::get);
 
     /** SecureRandom实例 */
-    public static RandomUtil SECURE = new RandomUtil(SECURE_RANDOM_SUPPLIER);
+    public static RandomUtil SECURE = new RandomUtil(SECURE_RANDOM_STRONG::get);
 
     private final Supplier<Random> randomSupplier;
 
@@ -38,6 +40,19 @@ public class RandomUtil {
 
     private Random random() {
         return randomSupplier.get();
+    }
+
+    /**
+     * 设置种子，注意仅在当前线程下生效，在当前线程下是单例
+     * 仅对INSECURE实例生效
+     *
+     * @param seed 种子值
+     */
+    public void setSeed(long seed) {
+        if (this == SECURE) {
+            throw new UnsupportedOperationException("SecureRandom not support set seed.");
+        }
+        random().setSeed(seed);
     }
 
     /**
@@ -53,6 +68,9 @@ public class RandomUtil {
      * @param exclusiveBound 随机数最大值（不包含）
      */
     public int nextInt(int exclusiveBound) {
+        if (exclusiveBound <= 0) {
+            throw new IllegalArgumentException("bound must be positive");
+        }
         return random().nextInt(exclusiveBound);
     }
 
@@ -67,7 +85,158 @@ public class RandomUtil {
             throw new IllegalArgumentException("min must be less than max");
         }
         int bound = exclusiveMax - inclusiveMin;
-        return inclusiveMin + random().nextInt(bound);
+        return inclusiveMin + nextInt(bound);
+    }
+
+    /**
+     * 集合中随机一项
+     *
+     * @param items   随机项集合
+     * @return 随机项
+     */
+    public <Item> Optional<Item> random(@Nonnull Collection<Item> items) {
+        if (items.isEmpty()) {
+            return Optional.empty();
+        }
+        if (items instanceof List) {
+            return random((List<Item>) items);
+        }
+        List<Item> temp = new ArrayList<>(items);
+        return random(temp);
+    }
+
+    /**
+     * 集合中随机一项
+     *
+     * @param items   随机项列表
+     * @return 随机项
+     */
+    public <Item> Optional<Item> random(@Nonnull List<Item> items) {
+        if (items.isEmpty()) {
+            return Optional.empty();
+        }
+
+        int randomIndex = nextInt(items.size());
+        return Optional.of(items.get(randomIndex));
+    }
+
+    /**
+     * 集合中随机多项(可重复)
+     *
+     * @param items   随机项列表
+     * @param num     随机数量
+     * @return 随机项列表
+     */
+    public <Item> List<Item> randomList(@Nonnull List<Item> items, int num) {
+        if (items.isEmpty() || num <= 0) {
+            return List.of();
+        }
+
+        List<Item> result = new ArrayList<>(num);
+        for (int i = 0; i < num; i++) {
+            int randomIndex = nextInt(items.size());
+            result.add(items.get(randomIndex));
+        }
+        return result;
+    }
+
+    /**
+     * 集合中随机多项(不可重复)
+     * Fisher-Yates洗牌算法
+     *
+     * @param items   随机项列表
+     * @param num     随机数量
+     * @throws IllegalArgumentException 数量大于集合提供的数量
+     * @return 随机项列表
+     */
+    public <Item> List<Item> randomSoleList(@Nonnull Collection<Item> items, int num) {
+        if (items.isEmpty() || num <= 0) {
+            return List.of();
+        }
+
+        if (num > items.size()) {
+            throw new IllegalArgumentException("num exceeds items.size()");
+        }
+
+        List<Item> copy = new ArrayList<>(items);
+        for (int i = 0; i < num; i++) {
+            //这里需要允许自己和自己交换
+            int swapIndex = i + nextInt(copy.size() - i);
+            Collections.swap(copy, i, swapIndex);
+        }
+        return num == items.size() ? copy : copy.subList(0, num);
+    }
+
+    /**
+     * 随机权重
+     *
+     * @param items   随机项
+     * @param weightFunction  随机项权重
+     * @throws IllegalArgumentException 随机项权重小于0
+     * @return 随机项
+     */
+    public <Item> Optional<Item> randomWeight(@Nonnull Collection<Item> items, @Nonnull ToIntFunction<Item> weightFunction) {
+        WeightData<Item> itemWeightData = prepareWeightData(items, weightFunction);
+        List<Item> itemList = itemWeightData.items();
+        int total = itemWeightData.total();
+        int[] prefixSum = itemWeightData.prefixSum();
+
+        if (total == 0) {
+            return Optional.empty();
+        }
+        int randomValue = nextInt(total) + 1;
+        int idx = Arrays.binarySearch(prefixSum, randomValue);
+        Item item = itemList.get(idx >= 0 ? idx : -idx - 1);
+        return Optional.of(item);
+    }
+
+    /**
+     * 随机权重列表
+     *
+     * @param items   随机项
+     * @param weightFunction  随机项权重
+     * @param num     随机数量
+     * @return 随机项列表
+     */
+    public <Item> List<Item> randomWeightList(@Nonnull Collection<Item> items, @Nonnull ToIntFunction<Item> weightFunction, int num) {
+        WeightData<Item> itemWeightData = prepareWeightData(items, weightFunction);
+        List<Item> itemList = itemWeightData.items();
+        int total = itemWeightData.total();
+        int[] prefixSum = itemWeightData.prefixSum();
+
+        List<Item> result = new ArrayList<>(num);
+        for (int i = 0; i < num; i++) {
+            int randomValue = nextInt(total) + 1;
+            int idx = Arrays.binarySearch(prefixSum, randomValue);
+            Item item = itemList.get(idx >= 0 ? idx : -idx - 1);
+            result.add(item);
+        }
+        return result;
+    }
+
+    /** 公共权重数据结构 */
+    private record WeightData<Item>(List<Item> items, int[] prefixSum, int total) {}
+
+    /**
+     * 解析权重数据
+     *
+     * @param items 权重随机集合
+     * @param weight 获取权重
+     * @return  权重数据
+     */
+    private <Item> WeightData<Item> prepareWeightData(Collection<Item> items, ToIntFunction<Item> weight) {
+        List<Item> itemList = new ArrayList<>(items);
+        int total = 0;
+        int[] prefixSum = new int[itemList.size()];
+        for (int i = 0; i < itemList.size(); i++) {
+            int w = weight.applyAsInt(itemList.get(i));
+            if (w < 0) {
+                throw new IllegalArgumentException("weight num must be positive");
+            }
+            total += w;
+            prefixSum[i] = total;
+        }
+        return new WeightData<>(itemList, prefixSum, total);
     }
 
     @Override
